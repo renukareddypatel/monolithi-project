d4121afd2db3f36065a0be343b922438ba4ee400
renukareddypatel <130661490+renukareddypatel@users.noreply.github.com>
2023-06-23 10:05:08 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ secret_key = "r0zLjEbC81PhzQ0QR94Qmr/o+aURRlw95M5XtnNY"
 }
 
 resource "aws_instance" "key" {
-ami = ""
+ami = "ami-012b9156f755804f5"
 instance_type = "t2.micro"
 vpc_security_group_ids = [aws_security_group.demo-sg.id]
 tags = {
